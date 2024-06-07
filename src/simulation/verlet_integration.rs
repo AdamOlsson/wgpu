53e3f91fa09efd015476f5cea04965c259e37b35
AdamOlsson <olsson.95_@hotmail.com>
2024-06-07 07:06:39 +0200

    working par naive collision system

--- src/simulation/verlet_integration.rs
+++ src/simulation/verlet_integration.rs
@@ -1,4 +1,4 @@
-use std::time::Instant;
+use std::{sync::{Arc, Mutex}, thread, time::Instant};
 use cgmath::{InnerSpace, Vector3};
 
 use crate::{renderer_backend::vertex::Vertex, shapes::circle::Circle};
@@ -27,12 +27,12 @@ pub struct VerletIntegration {
 impl VerletIntegration {
     pub fn new() -> Self {
         let common_radius = 0.01;
-        let num_instances = 10000;
+        let num_instances = 1000;
         let spawn_rate_ms = 50;
         let num_instances_to_render = 0;
         let prev_positions = [Vector3::new(-0.5, 0.2, 0.0); MAX_INSTANCES];
         let positions = [Vector3::new(-0.48, 0.22, 0.0); MAX_INSTANCES];
-        let acceleration = [Vector3::new(0.0, -18.82, 0.0); MAX_INSTANCES];
+        let acceleration = [Vector3::new(0.0, -90.82, 0.0); MAX_INSTANCES];
         let mass = [1.0; MAX_INSTANCES];
         let radius = [common_radius; MAX_INSTANCES];
 
@@ -110,11 +110,12 @@ impl VerletIntegration {
         }
 
         // Solve collisions
-        let num_substeps = 1;
+        let num_substeps = 6;
         let pos_slice = &mut self.positions[0..self.num_instances_to_render as usize];
         let rad_slice = &mut self.radius[0..self.num_instances_to_render as usize];
         for _ in 0..num_substeps {
-            Self::spatial_subdivision(pos_slice, rad_slice, self.num_instances_to_render);
+            //Self::spatial_subdivision(pos_slice, rad_slice, self.num_instances_to_render);
+            Self::par_spatial_subdivision(pos_slice, rad_slice, self.num_instances_to_render);
         }
 
         println!("fps: {} seconds, num_objects: {}", fps, self.num_instances_to_render);
@@ -167,7 +168,7 @@ impl VerletIntegration {
         for i in 1..(grid_width-1) {
             for j in 1..(grid_width-1){
                 let center_cell = i*grid_width + j;
-                let local_cell_ids = Self::get_neighboring_cell_ids(center_cell as u32, grid_width);
+                let local_cell_ids = Self::get_local_cell_ids(center_cell as u32, grid_width);
                 
                 // Collect local positions and radii
                 let mut local_positions: Vec<Vector3<f32>> = Vec::new();
@@ -193,7 +194,7 @@ impl VerletIntegration {
             }
         }
     }
-    fn get_neighboring_cell_ids(center_id: u32, grid_width: u32) -> [u32; 9] {
+    fn get_local_cell_ids(center_id: u32, grid_width: u32) -> [u32; 9] {
         let top_left = center_id - grid_width - 1;
         let top_center = center_id - grid_width;
         let top_right = center_id - grid_width + 1;
@@ -207,5 +208,98 @@ impl VerletIntegration {
             center_left, center_id, center_right,
             bottom_left, bottom_center, bottom_right
         ];
+    }
+
+    /// Run a parallell implementation of the spatial subdivision
+    /// 
+    /// Split the render window into columns and perform collision detection and
+    /// response in parallell. Starting from the 2nd column and 2nd row, each thread
+    /// performs spatial subdivision on neighbouring cells.
+    fn par_spatial_subdivision(
+        positions: &mut [Vector3<f32>], radius: &[f32], num_instances: u32
+    ){
+        if num_instances <= 1 {
+            return;
+        }
+        // Create grid with largest side equal to the largest diameter of the circles
+        let cell_size = radius.iter().take(num_instances as usize).fold(0.0, |acc, &r| f32::max(acc, r))*2.0;
+        let grid_width = (2.0/cell_size).ceil() as u32;
+
+        // Assign each circle to a cell
+        let mut cells: Vec<Vec<usize>> = vec![Vec::new(); (grid_width*grid_width) as usize];
+        for i in 0..num_instances as usize {
+            let pos = positions[i];
+            // Add 1.0 to offset all coordinates between 0.0 and 2.0
+            let x = ((pos.x + 1.0)/cell_size) as u32;
+            let y = ((pos.y + 1.0)/cell_size) as u32;
+            let cell_index = (y*grid_width + x) as usize;
+            cells[cell_index].push(i);
+        }
+
+        Self::run_pass(1, positions, radius, &cells, grid_width);
+        Self::run_pass(2, positions, radius, &cells, grid_width);
+        Self::run_pass(3, positions, radius, &cells, grid_width);
+    }
+
+    fn get_thread_context_cell_ids(center_column_id: u32, grid_width: u32) -> Vec<u32> {
+        let mut cell_ids = Vec::new();
+        let left_col_ids= ((center_column_id-1)..(grid_width*grid_width)).step_by(grid_width as usize);
+        let center_col_ids = ((center_column_id)..(grid_width*grid_width)).step_by(grid_width as usize);
+        let right_col_ids = ((center_column_id+1)..(grid_width*grid_width)).step_by(grid_width as usize);
+        cell_ids.extend(left_col_ids);
+        cell_ids.extend(center_col_ids);
+        cell_ids.extend(right_col_ids);
+        return cell_ids;
+    }
+
+    fn run_pass(
+        pass_num: u32, positions: &mut [Vector3<f32>], radius: &[f32],
+        cells: &Vec<Vec<usize>>, grid_width: u32
+    ) {
+        let mut localized_positions = Vec::new();
+        let mut localized_radius = Vec::new();
+        let mut localized_object_ids = Vec::new();
+        for column_id in (pass_num..grid_width-1).step_by(3) {
+            let thread_local_cell_ids = Self::get_thread_context_cell_ids(column_id, grid_width);
+            // Collect local information
+            let mut thread_local_positions = Vec::new();
+            let mut thread_local_radius = Vec::new();
+            let mut thread_local_object_ids = Vec::new();
+            for cell_id in thread_local_cell_ids.iter() {
+                for object_id in cells[*cell_id as usize].iter() {
+                    thread_local_positions.push(positions[*object_id]);
+                    thread_local_radius.push(radius[*object_id]);
+                    thread_local_object_ids.push(*object_id);
+                }
+            }
+            localized_positions.push(thread_local_positions);
+            localized_radius.push(thread_local_radius);
+            localized_object_ids.push(thread_local_object_ids);
+            
+        }
+        // Launch threads
+        let output = Arc::new(Mutex::new(Vec::new()));
+        thread::scope(|scope| {
+            for (thread_id, thread_local_positions) in localized_positions.iter_mut().enumerate() {
+                let thread_local_radius = &localized_radius[thread_id];
+                let global_output = Arc::clone(&output);
+                scope.spawn(move || {
+                    let num_local_instances = thread_local_positions.len() as u32;
+                    // FIXME: Replace with spatial_subdivion()
+                    Self::naive_collision_detection_and_resolution(&mut thread_local_positions[..], &thread_local_radius, num_local_instances);
+                    global_output.lock().unwrap().push((thread_id, thread_local_positions));
+                });
+            }
+        });
+        // Write back results
+        let inner_output = Arc::try_unwrap(output).unwrap().into_inner().unwrap();
+        for (thread_id, thread_local_positions) in inner_output {
+            let thread_local_object_ids = &localized_object_ids[thread_id];
+            let num_local_instances = thread_local_positions.len();
+            for k in 0..num_local_instances as usize {
+                let object_id = thread_local_object_ids[k] as usize;
+                positions[object_id] = thread_local_positions[k];
+            }
+        }
     }
 }
